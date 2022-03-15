dc3c46a071cef023e5eef4df88b31c1c456e3ce5
joeys1122 <josephs1122@gmail.com>
2022-03-14 22:05:29 -0400

    login and signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Signup(props) {
+  const navigate = useNavigate()
+  const [ credentials, setCredentials ] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    axios.post('https://bw50-secret-family-recipes.herokuapp.com/api/auth/register', credentials)
+      .then(res => {
+        navigate('/login');
+      })
+      .catch(err => console.log(err.message))
+  }
+
   return(
     <div>
       <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input 
+          type='text' 
+          name='username' 
+          onChange={handleChange} 
+          value={credentials.username}
+        />
+        <label>Password</label>
+        <input 
+          type='password' 
+          name='password' 
+          onChange={handleChange} 
+          value={credentials.password}
+        />
+        <button>Signup</button>
+      </form>
     </div>
   )
 }
